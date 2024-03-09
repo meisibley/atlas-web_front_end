d6e2fbd8c0280e7304820ce5a3f2b64f4204a328
meisibley <mkong99@yahoo.com>
2024-03-09 16:53:38 -0600

    task13 Binding + Closure

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,15 @@
+let user = {
+    hobby: "Calligraphy",
+    favoriteSport: "Hockey",
+    astrologicalSign: "Aries",
+    firstName: "Buillaume",
+    lastName: "Johns",
+    location: "Netherlands",
+    occupation: "Engineer",
+};
+function logWelcomeUser(welcomeString) {
+    console.log(`${welcomeString}, ${user.firstName}. Your occupation is: ${user.occupation}`);
+}
+let bindLogWelcomeUser = logWelcomeUser.bind(user);
+bindLogWelcomeUser("Welcome");
+bindLogWelcomeUser("Hello");
