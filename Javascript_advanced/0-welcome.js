1dc842eacb0fd252cba78d74090fd70098c4efc3
meisibley <mkong99@yahoo.com>
2024-03-08 18:50:58 -0600

    task1 Closure Scope Chain

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,5 +1,5 @@
 function welcome(firstName, lastName) {
-    const fullName = firstName + " " + lastName;
+    let fullName = firstName + " " + lastName;
     function displayFullName() {
         alert("Welcome " + fullName + "!");
     }
