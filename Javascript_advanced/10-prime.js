f587285f856218244697ec58a6757a19b6546376
meisibley <mkong99@yahoo.com>
2024-03-09 14:41:43 -0600

    task10 Excution stack n timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,27 @@
+function countPrimeNumbers() {
+    let count = 1;
+    let flag = 0;
+    for (let i = 3; i <= 100; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
+            }
+            else
+                flag = 1;
+        }
+        if (flag == 1)
+            count++;
+    }
+    console.log(count);
+    return count;
+}
+function runCountPNkTimes(n){
+    for (let k = 0; k < n; k++) {
+        countPrimeNumbers();
+    }
+}
+const startTime = performance.now();
+runCountPNkTimes(100);
+const endTime = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${endTime - startTime} milliseconds.`);
