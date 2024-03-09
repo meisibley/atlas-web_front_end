5aeb61104b46b4ce08a314660956772ff0db2c6b
meisibley <mkong99@yahoo.com>
2024-03-09 10:00:20 -0600

    task3 Closure and loops

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,13 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat){
+        return function() {
+            return seat;
+        }
+    }
+    const students = [];
+    for (let i = 0; i < numbersOfStudents; i++) {
+        students[i] = studentSeat(i + 1);
+    }
+    return students;
+}
+let classRoom = createClassRoom(10);
