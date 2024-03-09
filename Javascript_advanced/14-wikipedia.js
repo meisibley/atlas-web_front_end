898d71a3d883e6eef8c9d7398ba9d057bd384703
meisibley <mkong99@yahoo.com>
2024-03-09 17:26:58 -0600

    task14 Simple callback

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,19 @@
+function createElement(data) {
+    let paragraph = document.createElement("p");
+    let content = document.createTextNode(`${data}`);
+    p.appendChild(content);
+    document.querySelector("body").appendChild(p);
+};
+function queryWikipedia(callback) {
+    let newRequest = new XMLHttpRequest();
+    newRequest.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*", true);
+    newRequest.onload = function() {
+        if (this.status === 200) {
+            const data = JSON.parse(this.response);
+            const article = data.query.pages[21721040].extract;
+            callback(article);
+        }
+    }
+    newRequest.send();
+};
+queryWikipedia(createElement);
